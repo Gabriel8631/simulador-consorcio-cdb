7fd5746154afd31cc199c389d7e5f619b6cb53ec
Gabriel Moraes <128649049+Gabriel8631@users.noreply.github.com>
2026-03-31 14:44:14 -0300

    Implement SimuladorCDB class for investment simulation
    Adds a class to simulate investment growth in CDB.

--- src/SimuladorCDB.php
+++ src/SimuladorCDB.php
@@ -1,1 +1,40 @@
+<?php
 
+class SimuladorCDB
+{
+    /**
+     * Gera um array simulando a evolução de um investimento em CDB.
+     *
+     * @param float $aporteMensal
+     * @param float $taxaAnual Ex: 0.105 para 10.5%
+     * @param int $meses
+     * @return array
+     */
+    public static function calcularComAportes(float $aporteMensal, float $taxaAnual, int $meses): array
+    {
+        // Converte a taxa anual para taxa mensal equivalente
+        $taxaMensal = pow(1 + $taxaAnual, 1 / 12) - 1;
+
+        $dados = [];
+        $saldoAtual = 0.0;
+        $totalInvestido = 0.0;
+
+        for ($mes = 1; $mes <= $meses; $mes++) {
+            $saldoAtual += $aporteMensal;
+            $totalInvestido += $aporteMensal;
+
+            $rendimento = $saldoAtual * $taxaMensal;
+            $saldoAtual += $rendimento;
+
+            $dados[] = [
+                "mes" => $mes,
+                "aporte_mensal" => round($aporteMensal, 2),
+                "total_investido" => round($totalInvestido, 2),
+                "rendimento_mes" => round($rendimento, 2),
+                "patrimonio_acumulado" => round($saldoAtual, 2)
+            ];
+        }
+
+        return $dados;
+    }
+}
